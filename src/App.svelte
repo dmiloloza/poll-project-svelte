218be538b0153abe52d5bfd04c35c4393fef5d0f
dmiloloza <domagoj.miloloza@gmil.com>
2020-09-12 11:44:50 +0200

    feat: form component

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,11 @@
     import Header from "./components/Header.svelte";
     import Footer from "./components/Footer.svelte";
     import Tabs from "./shared/Tabs.svelte";
+    import CreatePollForm from "./components/CreatePollForm.svelte";
 
     //tabs
     let items = ['Current Polls', 'Add New Poll'];
-    let activeItem = 'Current Polls';
+    let activeItem = 'Add New Poll';
     const tabChange = (e) => activeItem = e.detail
 </script>
 
@@ -17,7 +18,7 @@
     {#if activeItem === 'Current Polls'}
         <p>Poll list component goes here</p>
     {:else if activeItem === 'Add New Poll'}
-        <p>New poll form component goes here</p>
+    <CreatePollForm/>
     {/if}
 </main>
 <Footer/>
