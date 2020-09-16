26e97e0d131afba6737ff4aaaa1a0c9bd4dcee3f
dmiloloza <domagoj.miloloza@gmil.com>
2020-09-16 10:22:35 +0200

    feat: #10 casting votes

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,22 +1,33 @@
 <script>
+    import {createEventDispatcher} from 'svelte'
+    const dispatch = createEventDispatcher();
+
     import Card from "../shared/Card.svelte";
     export let poll;
 
     //reactive values
     $: totalVotes = poll.votesA + poll.votesB;
 
+    //handling votes
+
+    //(option, id) => value
+    const handleVote = (option, id) => {
+        //option: option , id:id => property. 'vote' name of event
+        dispatch('vote', {option, id});
+    };
 </script>
+
 <Card>
     <div class="poll">
         <h3>
             {poll.question}
         </h3>
         <p>Total votes: {totalVotes}</p>
-        <div class="answer">
+        <div class="answer" on:click={() => handleVote('a', poll.id)}>
             <div class="percent percent-a"></div>
             <span>{poll.answerA} ({poll.votesA})</span>
         </div>
-        <div class="answer">
+        <div class="answer" on:click={() => handleVote('b', poll.id)}>
             <div class="percent percent-b"></div>
             <span>{poll.answerB} ({poll.votesB})</span>
         </div>
