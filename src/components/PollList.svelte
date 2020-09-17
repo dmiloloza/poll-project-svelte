2d1dbc75436af7fac92e7b37a8b9e3e4d4a6c37a
dmiloloza <domagoj.miloloza@gmil.com>
2020-09-17 09:34:16 +0200

    refactor: #30 sub/unsub to store

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,11 +1,26 @@
 <script>
+   // import {onMount, onDestroy} from 'svelte';
+    import PollStore from '../stores/PollStore'
     import PollDetails from "./PollDetails.svelte";
 
-    export let polls = [];
+    /*export let polls = [];
+
+    const unsub = PollStore.subscribe(data => polls = data)
+
+    onDestroy(() => {
+        //unsub from store
+        console.log('component destroyed');
+        unsub();
+    })*/
 </script>
 
+
+<!--$PollStore $ assume that we want to get the data from the Store(Array of polls)-->
+<!--$PollStore $ automatically subscribe us to that data ,
+and also it auto unsub. us from the store when component is destroys(switching between tabs)-->
+
 <div class="poll-list">
-    {#each polls as poll (poll.id) }
+    {#each $PollStore as poll (poll.id) }
         <div>
             <!--'vote' - name of event-->
             <!--custom event forwarding to the parent.Handler is not assigned-->
